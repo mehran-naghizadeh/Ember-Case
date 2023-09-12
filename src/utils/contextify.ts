efeb101bf81c892bd87ad29023faf5c7cecbad26
Mehran Naghizadeh <tarhineh@gmail.com>
2023-09-12 16:33:36 +0200

    Highlight all places affected by contextify

--- src/utils/contextify.ts
+++ src/utils/contextify.ts
@@ -20,6 +20,10 @@ export default function contextify(context: '@' | 'this') {
 
   const modifiedText = editor.document.getText().replace(searchPattern, replacePattern);
 
+
+  // Show the Find and Replace widget with the selected word
+  vscode.commands.executeCommand('editor.action.startFindReplaceAction');
+
   // Replace the entire document with the modified text
   editor.edit((editBuilder) => {
     const documentStart = new vscode.Position(0, 0);
@@ -28,4 +32,6 @@ export default function contextify(context: '@' | 'this') {
 
     editBuilder.replace(documentRange, modifiedText);
   });
+
+
 };
